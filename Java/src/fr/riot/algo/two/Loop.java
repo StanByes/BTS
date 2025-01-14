bfc8fbf08ec546c474c5eaab3c4e859c7519ef82
StanByes <stanbyes@gmail.com>
2025-01-14 12:24:22 +0100

    Remove comment on loop class for Java

--- Java/src/fr/riot/algo/two/Loop.java
+++ Java/src/fr/riot/algo/two/Loop.java
@@ -3,20 +3,15 @@ package fr.riot.algo.two;
 public class Loop {
 	public static void main(String[] args) {
 		// Q.11
-		/*
 		for (int i = 0; i <= 10; i++)
 			show(5, i);
-		*/
 		
 		// Q.9
-		/*
 		for (int i = 0; i <= 20; i++)
 			for (int j = 0; j <= 10; j++)
 				show(i, j);
-		*/
 		
 		// Q.10
-		/*
 		System.out.println("Table multiplication");
 		System.out.println("----------------------------------------------");
 		for (int i = 0; i <= 20; i++) {
@@ -27,27 +22,22 @@ public class Loop {
 
 			System.out.println("----------------------------------------------");
 		}
-		*/
 		
 		// Q.12
-		/*
 		System.out.println("décompte");
 		for (int i = 10; i >= 0; i--)
 			System.out.println(i);
 		System.out.println("Départ !");
-		*/
 		
 		// Q.13
-		/*
 		int i = 0;
 		while (i <= 10) {
 			show(5, i);
 			i++;
 		}
-		*/
 		
 		// Q.14
-		int i = 0;
+		i = 0;
 		do {
 			show(5, i);
 			i++;
